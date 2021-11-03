5a5ea83f7fd8e89efc3424c73dac9309cfc1c862
sbtah <shisastore@protonmail.com>
2021-11-03 20:51:29 +0100

    Learning ...

--- JS-Tutorial-1/index.js
+++ JS-Tutorial-1/index.js
@@ -4,8 +4,25 @@
 // let count = firtsBatch + secondBatch;
 // console.log(count);
 
-// Challange 1...
-let myAge = 36;
-const humanToDogRatio = 7;
-myDogAge = myAge * humanToDogRatio;
-console.log(myDogAge);
+// // Challange 1...
+// let myAge = 36;
+// const humanToDogRatio = 7;
+// myDogAge = myAge * humanToDogRatio;
+// console.log(myDogAge);
+
+
+// // Challange 2...
+// let bonusPoints = 50
+// bonusPoints += 50;
+// bonusPoints -= 75;
+// bonusPoints += 45;
+// console.log(bonusPoints);
+
+
+
+
+// Counter App
+
+function increment() {
+    console.log('Button was clicked!');
+};
