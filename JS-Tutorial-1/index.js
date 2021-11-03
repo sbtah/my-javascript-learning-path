e54fce2cf3cc4f2f6a112482313ad090d8a774f2
sbtah <shisastore@protonmail.com>
2021-11-03 21:41:24 +0100

    Finished part 1, I guess..

--- JS-Tutorial-1/index.js
+++ JS-Tutorial-1/index.js
@@ -5,24 +5,63 @@
 // console.log(count);
 
 // // Challange 1...
-// let myAge = 36;
-// const humanToDogRatio = 7;
-// myDogAge = myAge * humanToDogRatio;
+let myAge = 36;
+const humanToDogRatio = 7;
+myDogAge = myAge * humanToDogRatio;
 // console.log(myDogAge);
 
 
 // // Challange 2...
-// let bonusPoints = 50
-// bonusPoints += 50;
-// bonusPoints -= 75;
-// bonusPoints += 45;
+let bonusPoints = 50
+bonusPoints += 50;
+bonusPoints -= 75;
+bonusPoints += 45;
 // console.log(bonusPoints);
 
 
+// Race Counter challenge ...
+function countdown() {
+    for (let i = 5; i > 0; i--) {
+        console.log(i);
+
+    }
+    console.log('Go!');
+}
+// countdown()
+
+
+// Sum of all laps function challenge... 
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+
+function lapTime(x, y, z) {
+    return x + y + z;
+};
+// console.log(lapTime(lap1, lap2, lap3));
+
+
+
+// Increment number o laps challange ....
+let lapsCompleted = 0;
+
+function inrementLaps() {
+    lapsCompleted += 1;
+
+};
+inrementLaps();
+inrementLaps();
+inrementLaps();
+// console.log(lapsCompleted);
 
 
 // Counter App
+let count = 0;
+let countEl = document.getElementById('count-el')
+console.log(countEl)
 
 function increment() {
-    console.log('Button was clicked!');
+    count += 1;
+    document.getElementById('count-el').innerText = count;
 };
+
