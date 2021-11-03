a93777e166c3d2ec1ea0b29f6394523bac9d680f
sbtah <shisastore@protonmail.com>
2021-11-03 20:17:09 +0100

    Started new JS tutorial

---
+++ JS-Tutorial-1/index.js
@@ -0,0 +1,11 @@
+// document.getElementById('count-el').innerText = 5
+// let firtsBatch = 5;
+// let secondBatch = 12;
+// let count = firtsBatch + secondBatch;
+// console.log(count);
+
+// Challange 1...
+let myAge = 36;
+const humanToDogRatio = 7;
+myDogAge = myAge * humanToDogRatio;
+console.log(myDogAge);
