1805a80032c855c1161cc26a20070fd8212e6e3b
sbtah <shisastore@protonmail.com>
2021-11-11 21:37:11 +0100

    more learning....

--- JS-Tutorial-3/index.js
+++ JS-Tutorial-3/index.js
@@ -1,13 +1,26 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById('delete-btn');
 const ulEl = document.getElementById("ul-el");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
 
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
 }
+
+
+function render(leads) {
+    let listItems = '';
+    for (let i = 0; i < leads.length; i++) {
+
+        listItems += `<li><a href="http://${leads[i]}" target="_blank">${leads[i]}</a></li>`;
+
+    };
+    ulEl.innerHTML = listItems;
+};
 
 
 inputBtn.addEventListener("click", function () {
@@ -15,19 +28,13 @@ inputBtn.addEventListener("click", function () {
         myLeads.push(inputEl.value);
         inputEl.value = '';
         localStorage.setItem('myLeads', JSON.stringify(myLeads));
-        renderLeads()
-        console.log(localStorage.getItem('myLeads'));
+        render(myLeads);
     }
-
 });
 
-function renderLeads() {
-    let listItems = '';
-    for (let i = 0; i < myLeads.length; i++) {
 
-        listItems += `<li><a href="http://${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`;
-
-    };
-    ulEl.innerHTML = listItems;
-};
-
+deleteBtn.addEventListener("dblclick", function () {
+    myLeads = [];
+    localStorage.clear();
+    render(myLeads);
+});
